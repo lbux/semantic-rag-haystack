2b4de891cf1a641881870f5ac70a16f5b452e479
Ulises M <30765968+lbux@users.noreply.github.com>
2024-03-11 18:10:43 -0700

    add ranker and llama.cpp arguments

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from haystack.components.embedders import SentenceTransformersTextEmbedder
 from haystack_integrations.components.generators.llama_cpp import LlamaCppGenerator
 from haystack_integrations.components.retrievers.chroma import ChromaEmbeddingRetriever
 from haystack_integrations.document_stores.chroma import ChromaDocumentStore
+from haystack.components.rankers import TransformersSimilarityRanker
 
-from utils import serialize_generated_answer, read_input_json, read_serialized_generated_answer_v2
+from utils import serialize_generated_answer, read_input_json
 
 chat_template = """
 You are a teaching assistant for a course on Data Management.
 You will be provided with queries from students that have been posted on a discussion board, EdDiscussion.
-You will answer the queries using the data in the context.
-If you can not find the answer only using the context, do not provide an answer.
+You will answer the queries using the data in the syllabus.
+If you can not find the answer only using the syllabus, do not provide an answer.
 
 Here is an example of the expected answer based on a query:
 
 QUERY: "Are we able to use private edStem posts to contact you or is email preferred?"
-ANSWER: "Yes, you can use private edStem posts or email to contact us. Also, there is a section in the syllabus describing how/where to contact us that you may find helpful.:"
+ANSWER: "Yes, you can use private edStem posts or email to contact us. Also, there is a section in the syllabus describing how/where to contact us that you may find helpful."
+
 Context:
 {% for doc in documents %}
   {{ doc.content }}
@@ -29,7 +31,15 @@ generator = LlamaCppGenerator(
     model="models/mistral-7b-instruct-v0.2.Q8_0.gguf",
     n_ctx=12000,
     model_kwargs={"n_gpu_layers": -1},
-    generation_kwargs={"max_tokens": 128, "temperature": 0.7},
+    generation_kwargs={
+        # i pulled these from a random reddit post lol
+        # but they seem to work well?
+        "max_tokens": 128,
+        "temperature": 0.7,
+        "repeat_penalty": 1.176,
+        "top_k": 40,
+        "top_p": 0.1,
+    },
 )
 
 text_embedder = SentenceTransformersTextEmbedder(model="hkunlp/instructor-large")
@@ -43,15 +53,18 @@ document_store = ChromaDocumentStore(persist_path="chromaDB")
 rag_pipeline = Pipeline()
 rag_pipeline.add_component("text_embedder", text_embedder)
 rag_pipeline.add_component(
-    "embedder_retriever", ChromaEmbeddingRetriever(document_store=document_store)
+    "embedder_retriever",
+    ChromaEmbeddingRetriever(document_store=document_store, top_k=5),
 )
+rag_pipeline.add_component("ranker", TransformersSimilarityRanker())
 rag_pipeline.add_component("prompt_builder", PromptBuilder(template=chat_template))
 rag_pipeline.add_component("llm", generator)
 rag_pipeline.add_component("answer_builder", AnswerBuilder())
 
 
 rag_pipeline.connect("text_embedder", "embedder_retriever")
-rag_pipeline.connect("embedder_retriever", "prompt_builder")
+rag_pipeline.connect("embedder_retriever", "ranker")
+rag_pipeline.connect("ranker", "prompt_builder")
 rag_pipeline.connect("prompt_builder", "llm")
 rag_pipeline.connect("llm.replies", "answer_builder.replies")
 rag_pipeline.connect("llm.meta", "answer_builder.meta")
@@ -71,6 +84,7 @@ for prompt_dict in prompts:
             "text_embedder": {"text": question},
             "prompt_builder": {"query": question},
             "answer_builder": {"query": question},
+            "ranker": {"query": question},
         }
     )
     result_with_original = {"staff_answer": staff_answer, "generated_answer": result}
