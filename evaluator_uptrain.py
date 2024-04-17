7f8a18813752a8b180bf12de3039f0b9d8e93851
Ulises M <30765968+lbux@users.noreply.github.com>
2024-04-17 14:23:46 -0700

    more changes to local eval

--- evaluator_uptrain.py
+++ evaluator_uptrain.py
@@ -2,11 +2,19 @@ from uptrain import EvalLLM, Evals, Settings
 import json
 from utils import read_serialized_generated_answer, serialize_evaluation_results
 
-queries, documents, answers, staff_answers = read_serialized_generated_answer("")
+queries, documents, answers, staff_answers = read_serialized_generated_answer("generated_data_2024-04-15_22-15-39.json")
 
-settings = Settings(model='ollama/...')
+data = [
+    {
+        "question": queries[1],
+        "context": documents[1],
+        "response": answers[1],
+    }
+]
+
+settings = Settings(model='ollama/wizardlm2')
 eval_llm = EvalLLM(settings=settings)
 
-results = eval_llm.evaluate(project_name = "uptrain local test", data=data, checks=[Evals.FACTUAL_ACCURACY])
+results = eval_llm.evaluate(project_name = "uptrain local test", data=data, checks=[Evals.CONTEXT_RELEVANCE])
 
 print(results)
